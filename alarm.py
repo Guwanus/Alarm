c44508e77fe84f83455969e621df273a53530391
juan <juanmanuel.hol@student.fontys.nl>
2025-02-13 14:27:38 +0100

    Alarm af

--- alarm.py
+++ alarm.py
@@ -1,1 +1,7 @@
+getalinput = int(input("Vul een positief getal in:"))
 
+if getalinput >= 0:
+    for i in range(1, getalinput + 1):
+        print(f"Alarm {i}!")
+else:
+    print("Getal moet positief zijn!")
